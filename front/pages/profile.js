bb016f20ce77302c2a9ff9e141951bf048516a5d
lbg209 <lbg209@gmail.com>
2022-09-18 04:52:52 +0900

    change about.js

--- front/pages/profile.js
+++ front/pages/profile.js
@@ -54,7 +54,7 @@ const Profile = () => {
   return (
     <>
       <Head>
-        <title>내 프로필 | NodeBird</title>
+        <title>내 프로필 | Twitter</title>
       </Head>
       <AppLayout>
         <NicknameEditForm />
