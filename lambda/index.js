540763465df0fe885a5b5b6a1cc22a47a651a393
lbg209 <lbg209@gmail.com>
2022-10-26 03:57:33 +0900

    Add: thunbnail

--- lambda/index.js
+++ lambda/index.js
@@ -21,11 +21,13 @@ exports.handler = async (event, context, callback) => {
       .toFormat(requiredFormat)
       .toBuffer();
 
-    await s3.putObject({
-      Bucket,
-      Key: `thumb/${filename}`,
-      Body: resizedImage,
-    });
+    await s3
+      .putObject({
+        Bucket,
+        Key: `thumb/${filename}`,
+        Body: resizedImage,
+      })
+      .promise();
 
     console.log("put", resizedImage.length);
     return callback(null, `thumb/${filename}`);
