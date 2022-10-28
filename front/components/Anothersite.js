078a92fb562806026832132cd852c8443b06abc9
lbg209 <lbg209@gmail.com>
2022-10-28 21:34:30 +0900

    modify: src

--- front/components/Anothersite.js
+++ front/components/Anothersite.js
@@ -1,7 +1,5 @@
 import React from "react";
 import styled from "styled-components";
-import img1 from "../img/wevity.png";
-import img2 from "../img/thinkgood.jpg";
 
 const StyleComponent = styled.div`
   display: flex;
@@ -17,7 +15,7 @@ const StyleComponent = styled.div`
     img {
       margin-top: 5px;
       width: 150px;
-      height: 150px;
+      height: 100px;
     }
   }
 `;
@@ -29,14 +27,20 @@ const Anothersite = () => {
         <h5>다른 공모전 사이트</h5>
         <div className="image-box">
           <a href="https://www.wevity.com/" target="_blank" rel="noreferrer">
-            <img src={img1} alt="link"></img>
+            <img
+              src="https://www.wevity.com/images/logo_wevity.png"
+              alt="link"
+            ></img>
           </a>
           <a
             href="https://www.thinkcontest.com/"
             target="_blank"
             rel="noreferrer"
           >
-            <img src={img2} alt="link"></img>
+            <img
+              src="https://www.thinkcontest.com/assets/img/thinkgood_profile.jpg"
+              alt="link"
+            ></img>
           </a>
         </div>
       </div>
